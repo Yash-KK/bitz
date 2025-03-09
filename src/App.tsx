4c042f5a6a6d0fc24059bcf202490952fded3507
Yash-KK <reachyashk@gmail.com>
2025-03-09 08:49:08 +0530

    add-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import AppBar from "./components/AppBar";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-dvh mx-80">
       <AppBar />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 border-t-1 border-gray-700 p-4 mt-auto text-white">
+      Designed and Developed by <span className="font-bold">Yash</span>
+    </footer>
+  );
+};
+
+export default Footer;
